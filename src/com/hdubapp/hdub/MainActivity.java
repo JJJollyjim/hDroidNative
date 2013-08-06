b5d8e6721e00c28dbea5891fda1a89b670df96f5
Jamie McClymont <jamie.mcclymont@gmail.com>
2013-08-06 14:18:05 +1200

    Added ActionBarSherlock support

--- src/com/hdubapp/hdub/MainActivity.java
+++ src/com/hdubapp/hdub/MainActivity.java
@@ -1,10 +1,10 @@
 package com.hdubapp.hdub;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
